afff5264963898724d0d34f45414b186980db8ca
Avinash Bendigeri <avinashbendigeri@Avinashs-MacBook-Air.local>
2020-05-04 00:18:22 +0530

    Python Redis Implementation for the Product Catalogue

---
+++ testredis.py
@@ -0,0 +1,7 @@
+import redis
+# Create connection object
+r = redis.Redis(host='localhost', port=6379)
+# set a value for the foo object
+r.set('foo', 'bar')
+# retrieve and print the value for the foo object
+print(r.get('foo'))
